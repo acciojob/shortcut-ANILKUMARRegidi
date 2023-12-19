5e173c8cb9103b7561977c9be6b4804e6d154dfa
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-19 17:51:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function shortcut(s1, s2) {
-  // your code here
+  const s = s1[0] + s2[0];
+  return s;
+  
 }
 
-// Do not change the code below.
+// Do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
